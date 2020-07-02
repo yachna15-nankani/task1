ac395178643fddd506976c8dc85968942ae9ea9a
yachna15-nankani <66930365+yachna15-nankani@users.noreply.github.com>
2020-07-02 15:11:30 +0530

    update file

--- dashboard.php
+++
@@ -1,7 +0,0 @@
-<html>
-<body>
-<h3>Welcome to Hybrid Multicloud Computing </h3>
-<p>This is task1 given by world recordholder Mr Vimal Daga Sir</p>
-<img src="C:/Users/hp/Downloads/task1/images.png" width="300" height="300"/>
-</body>
-</html>
